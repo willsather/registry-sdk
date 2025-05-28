5d24b2a41575b8cca6da20860b0985c1ac858952
willsather <will@sather.ws>
2025-05-28 00:17:58 -0400

    chore: add tailwind styling to app

--- packages/registry-sdk/src/app/src/app.tsx
+++ packages/registry-sdk/src/app/src/app.tsx
@@ -2,9 +2,11 @@ import * as React from "react";
 
 export function App() {
   return (
-    <div>
-      <h1>Registry</h1>
-      <p>This is a registry!</p>
+    <div className="flex min-h-screen flex-col items-center justify-center gap-2 bg-gray-400 text-center">
+      <h1 className="font-extrabold font-mono text-primary">Registry</h1>
+      <p className="italic">
+        this is a registry running using `registry-sdk` and styled by tailwind
+      </p>
     </div>
   );
 }
